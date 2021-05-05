477f2cb929e719669e8ab2129fa031e215a93d6c
Khalid Mohamed <khalidmohamed93@gmail.com>
2021-05-05 10:49:16 -0700

    New Hedaer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>Yoga With Khalid</h1>
+      <Header/>
     </div>
   );
 }
